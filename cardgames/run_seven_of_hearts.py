569866eddd71a863604a53c4561fbd394c1d6add
Nikhil Lele <nik.lele@gmail.com>
2015-03-22 19:09:29 -0700

    move execution out of seven_of_hearts.py

---
+++ cardgames/run_seven_of_hearts.py
@@ -0,0 +1,22 @@
+import cardsource as cs
+from seven_of_hearts.seven_of_hearts import SevenOfHearts
+from seven_of_hearts.seven_of_hearts_player import HumanPlayer
+from seven_of_hearts.seven_of_hearts_ai import RandomPlayer
+
+if __name__ == '__main__':
+	game = SevenOfHearts(cs.Deck())
+
+	# 2 human players
+	# game.add_player(HumanPlayer("1", game))
+	# game.add_player(HumanPlayer("2", game))
+
+	# 4 random players
+	game.add_player(RandomPlayer("Random 1", game))
+	game.add_player(RandomPlayer("Random 2", game))
+	game.add_player(RandomPlayer("Random 3", game))
+	game.add_player(RandomPlayer("Random 4", game))
+
+	# run a single game
+	game.setup()
+	game.run()
+	game.finish()
--- cardgames/seven_of_hearts/seven_of_hearts.py
+++ cardgames/seven_of_hearts/seven_of_hearts.py
@@ -61,24 +61,3 @@ class SevenOfHearts(SheddingGame):
 
     def finish(self):
         print "finished the game in " + str(self.state['round']) + " rounds"
-
-if __name__ == '__main__':
-    from seven_of_hearts_player import HumanPlayer
-    from seven_of_hearts_ai import RandomPlayer
-
-    game = SevenOfHearts(cs.Deck())
-
-    # 2 human players
-    # game.add_player(HumanPlayer("1", game))
-    # game.add_player(HumanPlayer("2", game))
-
-    # 4 random players
-    game.add_player(RandomPlayer("Random 1", game))
-    game.add_player(RandomPlayer("Random 2", game))
-    game.add_player(RandomPlayer("Random 3", game))
-    game.add_player(RandomPlayer("Random 4", game))
-
-    # run a single game
-    game.setup()
-    game.run()
-    game.finish()
