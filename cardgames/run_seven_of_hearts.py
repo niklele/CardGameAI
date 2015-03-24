3d5b6631b28b5db74513849965619cd7a7911ea7
Nikhil Lele <nik.lele@gmail.com>
2015-03-23 17:11:06 -0700

    Heurisitic AI player that uses the card's distance from 7 and potential to lead to future cards in hand

--- cardgames/run_seven_of_hearts.py
+++ cardgames/run_seven_of_hearts.py
@@ -2,24 +2,28 @@ import logging as log
 import cardsource as cs
 from seven_of_hearts.seven_of_hearts import SevenOfHearts
 from seven_of_hearts.seven_of_hearts_player import HumanPlayer
-from seven_of_hearts.seven_of_hearts_ai import RandomPlayer
+from seven_of_hearts.seven_of_hearts_ai import RandomPlayer, HeuristicPlayer
 
 if __name__ == '__main__':
-	log.basicConfig(format='%(levelname)s: %(message)s', level=log.INFO)
+    log.basicConfig(format='%(levelname)s: %(message)s', level=log.DEBUG)
 
-	game = SevenOfHearts(cs.Deck())
+    game = SevenOfHearts(cs.Deck())
 
-	# 2 human players
-	# game.add_player(HumanPlayer("1", game))
-	# game.add_player(HumanPlayer("2", game))
+    # 2 human players
+    # game.add_player(HumanPlayer("Human 1", game))
+    # game.add_player(HumanPlayer("Human 2", game))
 
-	# 4 random players
-	game.add_player(RandomPlayer("Random 1", game))
-	game.add_player(RandomPlayer("Random 2", game))
-	game.add_player(RandomPlayer("Random 3", game))
-	game.add_player(RandomPlayer("Random 4", game))
+    # 4 random players
+    # game.add_player(RandomPlayer("Random 1", game))
+    # game.add_player(RandomPlayer("Random 2", game))
+    # game.add_player(RandomPlayer("Random 3", game))
+    # game.add_player(RandomPlayer("Random 4", game))
 
-	# run a single game
-	game.setup()
-	game.run()
-	game.finish()
+    # 1 human and 1 Heuristic AI
+    game.add_player(HumanPlayer("Human 1", game))
+    game.add_player(HeuristicPlayer("Heuristic 2", game))
+
+    # run a single game
+    game.setup()
+    game.run()
+    game.finish()
--- cardgames/seven_of_hearts/seven_of_hearts_ai.py
+++ cardgames/seven_of_hearts/seven_of_hearts_ai.py
@@ -1,9 +1,9 @@
 import logging as log
+import random
 import cardsource as cs
 from player import Player
 from seven_of_hearts import SevenOfHearts, Value
 from seven_of_hearts_player import SevenOfHeartsPlayer
-import random
 
 class RandomPlayer(SevenOfHeartsPlayer):
     """ An AI player that chooses randomly from legal moves """
@@ -22,3 +22,86 @@ class RandomPlayer(SevenOfHeartsPlayer):
     def update(self, update_msg):
         log.debug("Player " + self.name + " update: " + str(update_msg))
         self.game_state = update_msg
+
+class HeuristicPlayer(SevenOfHeartsPlayer):
+    """ 
+    An AI player that chooses randomly from legal moves
+    - distance to 7: heuristic for moves which block other players the most
+    - number of cards in hand in the same "block": heuristic for moves which help self
+    - implicit (1,1) tuning
+    """
+
+    def __init__(self, name, game):
+        super(HeuristicPlayer, self).__init__(name, game)
+        # number of cards in each block
+        self.blocks =  {'h': {'above': 0, 'below': 0},
+                        'd': {'above': 0, 'below': 0},
+                        's': {'above': 0, 'below': 0},
+                        'c': {'above': 0, 'below': 0} }
+
+    # def block_generator(self):
+    #     for suit in "hdsc":
+    #         for block in ['above', 'below']:
+    #             yield self.blocks[suit][block]
+
+    def distance(self, card):
+        # linear from 1/6 for 6,8 to 1 for A/K
+        return float(abs(Value(card) - 7)) / 6.0
+
+    def count_blocks(self):
+        # number of cards in each block
+        dividend = 0.0
+        for c in self.hand:
+            dividend += 1
+            if (Value(c) > 7):
+                self.blocks[c.suit]['above'] += 1
+            elif (Value(c) < 7):
+                self.blocks[c.suit]['below'] += 1
+            else:
+                self.blocks[c.suit]['above'] += 1
+                self.blocks[c.suit]['below'] += 1
+                dividend += 1
+
+        # self.blocks = map(lambda b: b / dividend, self.block_generator())
+
+        # divide all by dividend
+        for suit in "hdsc":
+            for block in ['above', 'below']:
+                self.blocks[suit][block] /= dividend
+
+        log.debug("blocks: " + str(self.blocks))
+
+    def block_value(self, card):
+        v = Value(card)
+        above = self.blocks[card.suit]['above']
+        below = self.blocks[card.suit]['below']
+        if (v > 7):
+            return above
+        elif (v < 7):
+            return below
+        else:
+            return above + below
+
+    def play(self):
+        self.count_blocks()
+
+        # choose card with highest sum of block val and distance
+        legal_moves = self.game.legal_moves(self.hand)
+        log.debug("legal moves in hand: " + str(legal_moves))
+
+        try:
+            # return tuple in choices that has the max value
+            choices = map(lambda c: (c, self.distance(c) + self.block_value(c)), legal_moves)
+            log.debug("choices: " + str(choices))
+
+            card,value = max(choices, key=lambda x: x[1])
+            log.debug("chose " + str(card) + " value: " + str(value))
+            return card
+        except IndexError:
+            log.debug("No legal moves! Skipping turn")
+            return cs.Card('X')
+
+    def update(self, update_msg):
+        log.debug("Player " + self.name + " update: " + str(update_msg))
+        self.game_state = update_msg
+
